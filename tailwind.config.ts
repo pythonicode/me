0808a1b72db487c13e801702c2d34d46114665f4
Anthony Riley <anthony.j.riley@gmail.com>
2024-10-06 16:36:19 -0400

    Initial setup with TailwindCSS and Shadcn

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+    darkMode: ['class'],
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
